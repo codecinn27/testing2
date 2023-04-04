b80e829dadfef958030968c3a981c0319aafc460
Acer <codecinnpro@gmail.com>
2023-04-04 09:03:47 +0800

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("hello world");
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+
+  app.get('/bye', (req, res) => {
+    res.send('byebye world')
+  })
+
+  app.get('/cinn', (req, res) => {
+    res.send('Hi cinn')
+  })
+  
+  app.get('/fariq', (req, res) => {
+    res.send('Hi fariq')
+  })
+  
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
